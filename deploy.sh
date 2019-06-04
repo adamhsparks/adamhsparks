fd72f3d22a2cd28742e3540e05ee2115e839a2d7
Adam Sparks <adamhsparks@gmail.com>
2019-06-04 16:00:00 +1000

    Remove extra fluff from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,6 @@
 echo "Your PATH is $PATH"
 echo "You are on branch $TRAVIS_BRANCH"
 echo "The working directory is $(pwd)"
-echo "The contents of contents/post are"
-ls content/post
-echo "The contents of contents/notes are"
-ls content/notes
 
 if [ "$TRAVIS_BRANCH" = "master" ]
 then
