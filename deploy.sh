9473475f819b2a06724b89b986d0e5a61da7f682
Adam Sparks <adamhsparks@gmail.com>
2019-06-04 15:44:41 +1000

    Update build procedure to mirror OPP

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-netlify deploy -a $NETLIFYKEY --prod --dir=public
+echo "Your PATH is $PATH"
+echo "You are on branch $TRAVIS_BRANCH"
+echo "The working directory is $(pwd)"
+echo "The contents of contents/post are"
+ls content/post
+echo "The contents of contents/notes are"
+ls content/notes
+
+if [ "$TRAVIS_BRANCH" = "master" ]
+then
+    echo "you are on master, deploying production."
+    netlify deploy -a $NETLIFYKEY --prod --dir=public
+else
+    echo "you are not on master, deploying preview."
+    netlify deploy -a $NETLIFYKEY --dir=public
+fi
